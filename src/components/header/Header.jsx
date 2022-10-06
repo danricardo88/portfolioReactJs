ccad88e5a08d022072dd875768a49bf7cf7b6010
danricardo88 <daniel.ricardo88@outlook.com>
2022-10-06 01:31:51 -0300

    feat: add react-icon, dev header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
@@ -10,6 +11,10 @@ const Header = () => {
         <h1>Daniel Ricardo</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
         <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
       </div>
     </header>
   )
---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocial = () => {
+  return (
+    <div className='header__socials'>
+      <a href="https://www.linkedin.com/in/danielricardo/" target='_blank' rel="noreferrer"></a>
+      <a href="https://www.linkedin.com/in/danielricardo/" target='_blank' rel="noreferrer"></a>  
+      <a href="https://www.linkedin.com/in/danielricardo/" target='_blank' rel="noreferrer"></a>
+    </div>
+  )
+}
+
+export default HeaderSocial
