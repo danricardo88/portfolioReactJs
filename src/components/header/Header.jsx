10e8853a07c53cae13317a76c4a5da2942256a46
danricardo88 <daniel.ricardo88@outlook.com>
2022-10-06 02:58:55 -0300

    header quase 90%

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './header.css';
 import CTA from './CTA';
 import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocial';
 
 const Header = () => {
   return (
@@ -11,10 +12,13 @@ const Header = () => {
         <h1>Daniel Ricardo</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
         <CTA />
+        <HeaderSocial />
 
         <div className="me">
           <img src={ME} alt="me" />
         </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {BsLinkedin} from 'react-icons/bs';
+import {GoMarkGithub} from 'react-icons/go'
 
 const HeaderSocial = () => {
   return (
     <div className='header__socials'>
-      <a href="https://www.linkedin.com/in/danielricardo/" target='_blank' rel="noreferrer"></a>
-      <a href="https://www.linkedin.com/in/danielricardo/" target='_blank' rel="noreferrer"></a>  
-      <a href="https://www.linkedin.com/in/danielricardo/" target='_blank' rel="noreferrer"></a>
+      <a href="https://linkedin.com" target='_blank' rel="noreferrer"><BsLinkedin /></a>
+      <a href="https://github.com" target='_blank' rel="noreferrer"><GoMarkGithub /></a>  
+      {/* <a href="https://.com" target='_blank'></a> */}
     </div>
   )
 }
 
-export default HeaderSocial
+export default HeaderSocial;
