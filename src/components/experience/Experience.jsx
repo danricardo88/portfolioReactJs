180829a1542eee4e7e1d0e0f1668f17c113cccdf
danricardo88 <daniel.ricardo88@outlook.com>
2022-10-12 09:35:14 -0300

    dev experience-ok

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,151 @@
 import React from 'react';
 import './experience.css';
+import { BsPatchCheckFill } from 'react-icons/bs';
+import { MdDownloading } from 'react-icons/md';
 
 function Experience() {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>React Testing Library</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        {/* END OF FRONTEND */}
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Docker</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <MdDownloading className="experience__details-icon" />
+              <div>
+                <h4>SQL</h4>
+                <small className="text-light">Loading...</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <MdDownloading className="experience__details-icon" />
+              <div>
+                <h4>Node JS</h4>
+                <small className="text-light">Loading...</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <MdDownloading className="experience__details-icon" />
+              <div>
+                <h4>Software Architecture</h4>
+                <small className="text-light">Loading...</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <MdDownloading className="experience__details-icon" />
+              <div>
+                <h4>Deployment</h4>
+                <small className="text-light">Loading...</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <MdDownloading className="experience__details-icon" />
+              <div>
+                <h4>TypeScript</h4>
+                <small className="text-light">Loading...</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <MdDownloading className="experience__details-icon" />
+              <div>
+                <h4>POO and Solid</h4>
+                <small className="text-light">Loading...</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <MdDownloading className="experience__details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Loading...</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <MdDownloading className="experience__details-icon" />
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Loading...</small>
+              </div>
+            </article>
+
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
